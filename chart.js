522eb24e5712001ed4ffa0eeaf84b1595c1fc60f
c0nscience <ber.bherzig@gmail.com>
2013-02-08 22:38:35 +0100

    Update chart.js

--- chart.js
+++ chart.js
@@ -18,7 +18,8 @@
                 {dateTime: moment("02/08/2013 20:20", dateTimeFormat), queueSize: 265424},
                 {dateTime: moment("02/08/2013 21:12", dateTimeFormat), queueSize: 264644},
                 {dateTime: moment("02/08/2013 21:50", dateTimeFormat), queueSize: 264094},
-                {dateTime: moment("02/08/2013 22:20", dateTimeFormat), queueSize: 263643}
+                {dateTime: moment("02/08/2013 22:20", dateTimeFormat), queueSize: 263643},
+                {dateTime: moment("02/08/2013 22:38", dateTimeFormat), queueSize: 263381}
             ],
             firstDataPoint = dataPoints[0],
             lastDataPoint = dataPoints[dataPoints.length-1];
